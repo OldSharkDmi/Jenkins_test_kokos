91b05169942d9d89cda02c7ee23a8c0e8b59e2fe
dmi <sssssssssssssssssssssssss@sss>
2024-12-03 23:53:11 +0300

    init

---
+++ src/main/java/org/example/kokos/KokosApplication.java
@@ -0,0 +1,13 @@
+package org.example.kokos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kokos/KokosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kokos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
